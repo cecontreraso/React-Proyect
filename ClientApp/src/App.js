7633da5cdf174658daa29854a8795b05f799bbcf
Cesar Contreras <cesar.contreras.or@gmail.com>
2023-09-22 17:25:55 -0300

    primer commit

---
+++ ClientApp/src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+const App = () => {
+
+    return (
+        <div className="container">
+            <h1>Prueba de intranet</h1>
+        </div>
+    )
+}
+export default App;
